3c6938cb5ba9802bf243673dc26c6ffcf0fff639
Joao Bueno <joaobueno121212@gmail.com>
2024-09-15 12:09:27 -0300

    erasing file and initializing values in matrix

--- Ap3/matrix.cpp
+++ Ap3/matrix.cpp
@@ -15,25 +15,35 @@ public:
         this->lines = lines;
         this->columns = columns;
 
+        //creating the matrix
         matrix = new double *[lines];
-
-        for (int i = 0; i < lines; i++)
-        {
-            matrix[i] = new double[columns];
+        for (int i = 0; i < lines; i++)         matrix[i] = new double[columns];
+        //initializing the values
+        for(int i = 0; i < lines; i++){
+            for(int j = 0; j < columns; j++){
+                matrix[i][j] = 0;
+            }
         }
     }
 
     void set_value(int line, int column, double value)
     {
-        if (line >= 0 && line < lines && column >= 0 && column < columns)
-        {
-            matrix[line][column] = value;
-        }
+        if (line >= 0 && line < lines && column >= 0 && column < columns)       matrix[line][column] = value;
+
     }
 
     double get_value(int line, int row)
     {
         return matrix[line][row];
+    }
+
+    void printMatrix(){
+        for(int i = 0; i < lines; i++){
+            for(int j = 0; j < columns; j++){
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
     }
 
     ~Matrix()
@@ -48,10 +58,15 @@ public:
 
 int main()
 {
-    Matrix M(5, 3);
+    Matrix M(5, 4);
 
     M.set_value(2,2, 5.5);
+    M.set_value(0,0, 23);
+    M.set_value(0,0, 32);
+    M.set_value(0,1, 5);
+
     cout << M.get_value(2,2) << endl;
+    M.printMatrix();
 
 
     return 0;
